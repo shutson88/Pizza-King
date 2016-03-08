c8fc669f07985e2d2bf82838ea1628a69348dc29
Seth <shutson@tamu.edu>
2016-03-08 16:38:48 -0600

    I guess this works

--- models.py
+++ models.py
@@ -10,7 +10,7 @@
 
 from django.db import models
 # from polymorphic import PolymorphicModel
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 
@@ -34,11 +34,11 @@ class Address(models.Model):
         return '%s %s %s %s %s' % (self.address1, self.address2, self.city, self.state, self.postal_code)
 
 
-class User(AbstractUser):
+class User(User):
     # A registered user the Fish Store. Can have User, Seller, or Admin roles.
     phone = models.TextField(max_length=22)
     used_coupon =models.BooleanField(default=False)
-    store = models.ForeignKey("Store", null=True, black=True)
+    store = models.ForeignKey("Store", null=True, blank=True)
 
     class Meta:
         ordering = ['last_name', 'first_name']
@@ -54,7 +54,7 @@ class Store(models.Model):
 
 class Pizza(models.Model):
     # May be sold
-    name = models.TextField(max_length=255)
+    name = models.TextField(max_length=150)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='images')
@@ -76,7 +76,7 @@ class Topping(models.Model):
         ordering = ['name']
 
     def __str__(self):
-        return '%s %s %s' % (self.name, self.description, self.price)
+        return '%s %s' % (self.name, self.price)
 
 
 class PizzaTopping(models.Model):
