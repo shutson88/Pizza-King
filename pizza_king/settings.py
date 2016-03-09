a6b388fffab228255d79ae5841054981916272b0
Jonathan Hodnett <jofinh@gmail.com>
2016-03-09 10:12:13 -0600

    db2 setup

--- pizza_king/settings.py
+++ pizza_king/settings.py
@@ -106,7 +106,7 @@ WSGI_APPLICATION = 'pizza_king.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
     }
 }
 
